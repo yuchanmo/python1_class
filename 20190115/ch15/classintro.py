17a1cf396aa7c5c2f07c1b1bef26463525479f4b
yuchanmo <mojjijji@gmail.com>
2019-01-15 15:58:54 +0900

    왜 임포트가 안되는가

--- 20190115/ch15/classintro.py
+++ 20190115/ch15/classintro.py
@@ -1,7 +1,39 @@
 class Ph:
-    def printHam():
+    def __init__(self):
+        self.y = 5
+        self.z = 5
+    def printHam(self):
         print('ham')
 
 
-p=Ph()
+class Hero:
+    def __init__(self,name):
+        self.name = name
+        self.health = 100
+    
+    def eat(self,food):
+        if food == 'apple':
+            self.health -=100
+        elif food =='ham':
+            self.health+=20
+
+bob = Hero('bob')
+print(bob.name)
+print(bob.health)
+bob.eat('ham')
+print(bob.health)
+
+p = Ph()
 p.printHam()
+print(p.y)
+print(p.z)
+
+
+class Service:
+    def sum(self,a,b):
+        result = a+b
+        print('%s + %s = %s 입니다'%(a,b,result))
+        print("{0} +{1} = {2}".format(a,b,result))
+
+pey = Service()
+pey.sum(1,1)
---
+++ moduletest/service.py
@@ -0,0 +1,5 @@
+class Service:
+    def __init__(self, a,b):
+        self.a = 3
+        self.b = 3
+    
